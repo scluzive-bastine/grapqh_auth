82d61d6d4be616dfc4b550bbe783969ce06f09cb
Sabastine <toshfuture@gmail.com>
2024-01-27 22:43:49 +0000

    layout updated

--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -1,21 +1,9 @@
 "use client"
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import React from "react"
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  const [queryClient] = React.useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 60 * 1000,
-          },
-        },
-      })
-  )
-
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  const queryclient = new QueryClient()
+  return <QueryClientProvider client={queryclient}>{children}</QueryClientProvider>
 }
 
 export default Providers
