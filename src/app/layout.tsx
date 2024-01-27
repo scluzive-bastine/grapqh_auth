a8ac761f4e36f734c7acc191c4bab5cf7ddf91b8
Sabastine <toshfuture@gmail.com>
2024-01-27 22:46:31 +0000

    layout updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,23 @@
 import type { Metadata } from "next"
-import { Inter as FontSans } from "next/font/google"
+import { Inter } from "next/font/google"
 import "./styles/globals.css"
 import Providers from "./components/Providers"
 import { cn } from "@/lib/utils"
 import Navbar from "./components/Navbar"
 import { Toaster } from "@/components/ui/toaster"
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "GraphQL Authentication",
   description: "Authentication with Graphql and Nextjs",
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
       <body
-        className={cn("bg-white text-gray-900 antialiased light min-h-screen", fontSans.className)}
+        className={cn("bg-white text-gray-900 antialiased light min-h-screen", inter.className)}
       >
         <Providers>
           <Navbar />
