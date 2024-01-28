dec666b6c7881b98e97ce54bee436e7d363916ce
Sabastine <toshfuture@gmail.com>
2024-01-28 00:20:46 +0000

    fixed playwright

--- tests/registerPage.spec.ts
+++ tests/registerPage.spec.ts
@@ -1,13 +1,18 @@
 import { test, expect } from "@playwright/test"
+import { faker } from "@faker-js/faker"
+
+const username = faker.internet.userName()
+const email = faker.internet.email()
+const password = "password"
 
 test("Register form works correctly", async ({ page }) => {
   // Navigate to the login page
   await page.goto("/register")
 
   // Fill in the login form
-  await page.fill("input[name=username]", "doe")
-  await page.fill("input[name=email]", "johndoe@example.com")
-  await page.fill("input[name=password]", "password")
+  await page.fill("input[name=username]", username)
+  await page.fill("input[name=email]", email)
+  await page.fill("input[name=password]", password)
 
   // Submit the form
   await page.click("button")
